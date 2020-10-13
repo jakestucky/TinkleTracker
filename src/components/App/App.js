e2ab694dd52aec1695b87d620e711ef3a726b3d4
Jake Stucky <jakestucky@gmnail.com>
2020-10-13 14:08:01 -0500

    cleaning up files and header basics are created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   HashRouter as Router,
   Route,
@@ -6,25 +6,24 @@ import {
   Switch,
 } from 'react-router-dom';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
-import AuthSandbox from '../AuthSandbox/AuthSandbox';
-import PetList from '../Pets/PetList';
+import Header from '../Header/Header';
 
 import './App.css';
 
 class App extends Component {
-  componentDidMount () {
+  componentDidMount() {
     this.props.dispatch({
-      type: 'FETCH_USER'
+      type: 'FETCH_USER',
     });
   }
 
@@ -32,45 +31,23 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <Header />
           <Nav />
+
           <Switch>
             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/sandbox" />
+            <Redirect exact from='/' to='/home' />
             {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
+            <Route exact path='/about' component={AboutPage} />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
-              exact
-              path="/home"
-              component={UserPage}
-            />
+            <ProtectedRoute exact path='/home' component={UserPage} />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
-
-            <Route 
-              exact 
-              path="/sandbox"
-              component={AuthSandbox}
-            />
-
-            <ProtectedRoute
-              exact
-              path="/pets"
-              component={PetList}
-            />
+            <ProtectedRoute exact path='/info' component={InfoPage} />
 
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
@@ -78,7 +55,8 @@ class App extends Component {
           <Footer />
         </div>
       </Router>
-  )}
+    );
+  }
 }
 
 export default connect()(App);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './Footer.css';
-
+import './Header.css';
+import star from './star1.png';
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const Footer = () => (
-  <footer>&copy; Designed and Created by Jake Stucky 2020</footer>
+const Header = () => (
+  <header>
+    <div className='head'>
+      <img height='150px' src={star} />
+      <h1>TinkleTracker</h1>
+    </div>
+  </header>
 );
 
-export default Footer;
+export default Header;
