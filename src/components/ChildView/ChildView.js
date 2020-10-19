6d8f53038f81152b3d1b6f3ce3ea570c11f3717c
Jake Stucky <jakestucky@gmnail.com>
2020-10-19 10:23:14 -0500

    updated some styling and images

--- src/components/ChildView/ChildView.js
+++ src/components/ChildView/ChildView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './ChildView.css';
-import num1 from './pee.jpg';
+import num1 from './urine1.png';
 import num2 from './Poop_Emoji.png';
 import wet from './sad.png';
 import dry from './smiley.png';
@@ -11,16 +11,16 @@ class ChildView extends Component {
     return (
       <div>
         <div className='buttonGrid'>
-          <button className='event-button'>
+          <button className='event-button-child'>
             <img height='200' src={num1} />
           </button>
-          <button className='event-button'>
+          <button className='event-button-child'>
             <img height='200' src={num2} />
           </button>
-          <button className='event-button'>
+          <button className='event-button-child'>
             <img height='200' src={wet} />
           </button>
-          <button className='event-button'>
+          <button className='event-button-child'>
             <img height='200' src={dry} />
           </button>
         </div>
