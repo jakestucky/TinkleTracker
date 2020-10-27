c698f2b8ad399fbe7c90cae09ba015c146fc5f21
Jake Stucky <jakestucky@gmnail.com>
2020-10-26 21:58:44 -0500

    created basic testing structure

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -26,6 +26,12 @@ if (process.env.DATABASE_URL) {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
+}
+//added database to use for testing
+else if (process.env.TEST) {
+  config = {
+    database: 'tinkle_tracker_test',
+  };
 } else {
   config = {
     host: 'localhost', // Server hosting the postgres database
---
+++ tests/server/users.test.js
@@ -0,0 +1,5 @@
+const { TestScheduler } = require('jest');
+
+test('Should fail if not logged in', () => {});
+
+test('Should return the goal for user id', () => {});
